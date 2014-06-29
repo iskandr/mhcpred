4342aa0040969214d6d33651547691ae97276729
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2014-06-29 19:21:32 -0400

    subset of psuedosequence positions

--- clean_mhc_seqs.py
+++ clean_mhc_seqs.py
@@ -1,7 +1,7 @@
 
 strings = []
 alleles = []
-with open('MHC_aa_seqs.txt') as f:
+with open('netmhcpan_pseudo_mhc.txt', 'r') as f:
 	text = f.read()
 	for line in text.split('\n'):
 		parts = [x for x in line.split(' ') if len(x) > 0]
@@ -23,10 +23,17 @@ for i, c in enumerate(first):
 		total += 1
 	fraction =  float(count)/total
 	print i, c, count, "/", total, fraction 
-	if fraction > 0.99:
+	if fraction > 0.95:
 		print "-- Skipping position %d" % i 
 		keep_position[i] = False 
 
+
 import numpy as np 
 print "# skipped = %d" % (len(keep_position) - np.sum(keep_position))
+
+with open('MHC_aa_seqs.csv', 'w') as f:
+	f.write("Allele,Residues\n")
+	for i, (seq, allele) in enumerate(zip(strings,alleles)):
+		subseq = "".join([c for i, c in enumerate(seq) if keep_position[i]])
+		f.write("%s,%s\n" % (allele, subseq))
 
