ecdc7c5ce770df9dd587da0e3a1b8c575c253c4d
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2014-10-13 17:22:18 -0400

    longer deletions in simple aligner

--- align_seqs.py
+++ align_seqs.py
@@ -1,6 +1,7 @@
 import sys
 import argparse
 import collections
+from itertools import izip
 
 from parsing import parse_fasta_mhc_dirs, parse_aa_table
 
@@ -16,79 +17,136 @@ parser.add_argument(
 )
 parser.add_argument(
     "--reference-allele",
-    default = "HLA-A*02:01",
+    default="HLA-C*08:38",
     help="Allele to align against",
 )
 
 parser.add_argument(
     "--output-file",
-    type = str,
-    help = "Path to output FASTA file",
+    type=str,
+    help="Path to output FASTA file",
     default="aligned_output.fa"
 )
 
 parser.add_argument(
-    "--exclude",
-    type = str,
-    help = "Exclude alleles which contain this substring",
-    default = "",
+    "--require-substring",
+    type=str,
+    default=[],
+    help="Allele name must contain this substring"
 )
 
-def align(ref, seq):
-    """
-    Delete positions from seq one at a time until same length as ref
-    """
-    n = len(ref)
+parser.add_argument(
+    "--exclude",
+    type=str,
+    help="Exclude alleles which contain this substring",
+    default="",
+)
 
-    assert len(seq) >= n, (ref, seq, len(ref), len(seq))
-    n_extra = n - len(seq)
+parser.add_argument(
+    "--coeff-file",
+    type=str,
+    default="BLOSUM50",
+    help="File containing BLOSUM matrix coefficients"
+)
 
-    blosum50 = parse_aa_table("BLOSUM50")
+class Aligner(object):
 
-    # choose best start position (trimming off large insertions at the
-    # beginning of the sequence
-    best_score = -1000
-    aligned = seq
-    for i in xrange(len(seq) - len(ref)):
-        score = sum(blosum50[x+y] for (x,y) in zip(seq[i:], ref))
-        if score > best_score:
-            best_score = score
-            aligned = seq[i:]
+    def __init__(
+            self,
+            coefficient_matrix_filename="BLOSUM50",
+            exact_match_bonus = 0):
+        self.coeffs = parse_aa_table(coefficient_matrix_filename)
+        self.nested_coeffs_dict = {}
+        for k,v in self.coeffs.iteritems():
+            x, y = k
+            if x not in self.nested_coeffs_dict:
+                self.nested_coeffs_dict[x] = {}
+            if x == y:
+                v += exact_match_bonus
+            self.nested_coeffs_dict[x][y] = v
 
-    while len(aligned) > n:
-        # chpoose a position to delete
-        best = None
-        best_score = -1000
-        for i in xrange(len(aligned)):
-            candidate = aligned[:i] + aligned[i+1:]
-            score = sum(blosum50[x+y] for (x,y) in zip(candidate, ref))
-            if score > best_score:
-                best = candidate
-                best_score = score
-        aligned = best
-    return aligned
+    def align(self, ref, seq, maxdels_per_iter=50):
+        """
+        Delete positions from seq one at a time until same length as ref
+        """
+        n = len(ref)
+
+        assert len(seq) >= n, (ref, seq, len(ref), len(seq))
+        n_extra = n - len(seq)
+
+        coeffs = self.nested_coeffs_dict
+
+        # choose best start position (trimming off large insertions at the
+        # beginning of the sequence
+        best_score = 0
+        aligned = seq
+
+        for i in xrange(len(seq) - len(ref) + 1):
+            candidate = seq[i:]
+            n_matches = sum(x==y for (x,y) in izip(candidate, ref))
+            if n_matches > best_score:
+                best_score = n_matches
+                aligned = candidate
+
+
+        # after a hopefully faster first phase of getting rid of a
+        # non-matching prefix, follow up with approximate-match iterative
+        # deletions until same length as reference
+        while len(aligned) > n:
+            # choose a range of positions to delete
+            best = None
+            best_score = -np.inf
+            best_score_len = 0
+            best_i = 0
+            best_j = 0
+            for i in xrange(len(aligned)):
+                for j in xrange(i+1, min(len(aligned), i+maxdels_per_iter)):
+                    candidate = aligned[:i] + aligned[j:]
+                    score = sum(coeffs[x][y] for (x,y) in izip(candidate, ref))
+                    delsize = j - i
+                    if score > best_score:
+                        best = candidate
+                        best_score = score
+                        best_score_len = delsize
+                    elif score == best_score and delsize  > best_score_len:
+                        best = candidate
+                        best_score = score
+                        best_score_len = delsize
+            aligned = best
+
+        return aligned
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
     exclude_alleles = [allele for allele in args.exclude.split(",") if allele]
+    require_substrings = [
+        substr
+        for substr in args.require_substring.split(",")
+        if substr]
     seqs = parse_fasta_mhc_dirs(
         [args.dir],
-        exclude_alleles=exclude_alleles)
+        exclude_allele_substrings=exclude_alleles,
+        require_allele_substrings=require_substrings)
     assert args.reference_allele in seqs, seqs.keys()
     ref = seqs[args.reference_allele]
+
     print "Reference", ref
     print "Length", len(ref)
+
     aligned = collections.OrderedDict()
+    aligner = Aligner(args.coeff_file)
+
     for allele in sorted(seqs.keys()):
         seq = seqs[allele]
+
         if len(seq) < len(ref):
             print "Skipping %s (len=%d)" % (allele, len(seq))
         elif any(c in seq for c in ("X", "Z", "B", "J")):
             print "Incomplete sequence for %s" % allele
         else:
             print ">", allele
-            aligned_seq = align(ref, seq)
+            aligned_seq = aligner.align(ref, seq)
             mismatch = "".join(
                 x if x != y else "_" for (x,y) in zip(aligned_seq,ref)
             )
--- parsing.py
+++ parsing.py
@@ -1,9 +1,12 @@
 from immuno.common import find_paths
 from Bio.SeqIO.FastaIO import FastaIterator
+
+
 def parse_fasta_mhc_dirs(
         dirs,
         min_length=0,
-        exclude_alleles=None):
+        exclude_allele_substrings=None,
+        require_allele_substrings=None):
     paths = []
     for d in dirs:
         paths.extend(
@@ -13,9 +16,14 @@ def parse_fasta_mhc_dirs(
     return parse_fasta_mhc_files(
             paths,
             min_length=min_length,
-            exclude_alleles=exclude_alleles)
+            exclude_allele_substrings=exclude_allele_substrings,
+            require_allele_substrings=require_allele_substrings)
 
-def parse_fasta_mhc_files(paths, min_length=0, exclude_alleles=[]):
+def parse_fasta_mhc_files(
+        paths,
+        min_length=0,
+        exclude_allele_substrings=[],
+        require_allele_substrings=[]):
     seqs = {}
     for path in paths:
         with open(path, 'r') as f:
@@ -30,11 +38,14 @@ def parse_fasta_mhc_files(paths, min_length=0, exclude_alleles=[]):
                         allele = "HLA-" + allele
                 if allele.endswith("N") or allele.endswith("Q"):
                     continue
-                if exclude_alleles and any(
-                        exclude in allele for exclude in exclude_alleles):
-                    print "Skipping excluded allele", allele
+                if exclude_allele_substrings and any(
+                        exclude in allele
+                        for exclude in exclude_allele_substrings):
                     continue
-
+                if require_allele_substrings and all(
+                        substr not in allele
+                        for substr in require_allele_substrings):
+                    continue
                 allele = allele.replace("_", "*")
 
                 seq = str(record.seq)
@@ -62,17 +73,22 @@ def parse_aa_table(filename, coeff_type = int):
         lines = [line for line in lines if not line.startswith("#")]
         # drop CR endline characters
         lines = [line.replace("\r", "") for line in lines]
+        # skip empty lines
+        lines = [line for line in lines if line]
+
         labels = lines[0].split()
-        assert len(labels) == 20, \
-            "Expected 20 amino acids but first line '%s' has %d fields" % (
+
+        assert len(labels) >= 20, \
+            "Expected 20+ amino acids but first line '%s' has %d fields" % (
             lines[0],
             len(labels)
             )
         coeffs = {}
         for line in lines[1:]:
+
             fields = line.split()
-            assert len(fields) == 21, \
-                "Expected letter + 20 coefficients but '%s 'has %d fields" % (
+            assert len(fields) >= 21, \
+                "Expected AA and 20+ coefficients but '%s' has %d fields" % (
                     line, len(fields)
                 )
             x = fields[0]
